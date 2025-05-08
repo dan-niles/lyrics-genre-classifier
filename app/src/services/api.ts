9127449c80a9326d2d4399e42a460ad7d90edc9f
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2025-05-09 03:58:37 +0530

    Refactor lyrics prediction to use cleaned lyrics for API request

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -15,16 +15,18 @@ export interface GenrePrediction {
 export async function predictLyrics(
 	lyrics: string
 ): Promise<GenrePrediction | null> {
+	const cleanedLyrics = clean_lyrics(lyrics);
+
 	try {
 		const response = await fetch("http://localhost:9090/lyrics/predict", {
 			method: "POST",
 			headers: {
 				"Content-Type": "text/plain",
 			},
-			body: lyrics,
+			body: cleanedLyrics,
 		});
 
-        console.log(response);
+		console.log(response);
 
 		if (!response.ok) {
 			throw new Error("Failed to predict lyrics");
@@ -37,3 +39,25 @@ export async function predictLyrics(
 		return null;
 	}
 }
+
+const clean_lyrics = (lyrics: string): string => {
+	// Remove any extra spaces
+	let cleanedLyrics = lyrics.replace(/\s+/g, " ").trim();
+
+	// Remove any leading or trailing spaces
+	cleanedLyrics = cleanedLyrics.replace(/^\s+|\s+$/g, "");
+
+	// convert to lowercase
+	cleanedLyrics = cleanedLyrics.toLowerCase();
+
+	// remove [] and () and their contents
+	cleanedLyrics = cleanedLyrics.replace(/\[.*?\]|\(.*?\)/g, "");
+
+	// remove newlines
+	cleanedLyrics = cleanedLyrics.replace(/(\r\n|\n|\r)/gm, " ");
+
+	// remove redundant spaces
+	cleanedLyrics = cleanedLyrics.replace(/\s+/g, " ");
+
+	return cleanedLyrics;
+};
